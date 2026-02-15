4dd286e74129db9b29d3d0cb32f96f4fec274872
Sasha Horlach <2018gorlach@gmail.com>
2026-02-15 19:12:09 +0200

    feat: create test user and create JwtAuthenticationFilter

--- src/main/java/com/pejjok/blog/config/SecurityConfig.java
+++ src/main/java/com/pejjok/blog/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.pejjok.blog.config;
 
+import com.pejjok.blog.domain.entities.UserEntity;
+import com.pejjok.blog.repositories.UserRepository;
+import com.pejjok.blog.security.BlogUserDetailsServices;
+import com.pejjok.blog.security.JwtAuthenticationFilter;
+import com.pejjok.blog.services.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,16 +12,44 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    JwtAuthenticationFilter authenticationFilter(AuthenticationService authenticationService){
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository){
+        BlogUserDetailsServices blogUserDetailsServices =  new BlogUserDetailsServices(userRepository);
+
+        // Create test user, before sign up are implementing
+        String email = "user@test.com";
+        userRepository.findByEmail(email).orElseGet(()->{
+            UserEntity newUser = UserEntity.builder()
+                    .name("Test user")
+                    .email(email)
+                    .password("{noop}password")
+                    .build();
+            return userRepository.save(newUser);
+        });
+
+
+        return blogUserDetailsServices;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,JwtAuthenticationFilter authenticationFilter) throws Exception{
         http.
                 authorizeHttpRequests(auth->auth
+                        .requestMatchers(HttpMethod.POST,"/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/tags/**").permitAll()
@@ -25,7 +58,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                )
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
