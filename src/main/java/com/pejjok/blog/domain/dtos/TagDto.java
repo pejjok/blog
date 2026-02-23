b082e1d374da777c0931fdb2a943d6c3b77a9f75
Sasha Horlach <2018gorlach@gmail.com>
2026-02-23 11:06:03 +0200

    refactor: add builder annotation to tag dto

--- src/main/java/com/pejjok/blog/domain/dtos/TagDto.java
+++ src/main/java/com/pejjok/blog/domain/dtos/TagDto.java
@@ -1,6 +1,7 @@
 package com.pejjok.blog.domain.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class TagDto {
     private UUID id;
     private String name;
